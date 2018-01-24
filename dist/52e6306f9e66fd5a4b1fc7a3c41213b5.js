a4118d63c125bc7047d9395be3e79657e559582f
okram ovic <okram@protonmail.ch>
2018-01-24 21:41:11 +0100

    adding auto-modified dist files

--- dist/52e6306f9e66fd5a4b1fc7a3c41213b5.js
+++ dist/52e6306f9e66fd5a4b1fc7a3c41213b5.js
@@ -149,7 +149,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
-  var ws = new WebSocket('ws://localhost:55559/');
+  var ws = new WebSocket('ws://localhost:61375/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
